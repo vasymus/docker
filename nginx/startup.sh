7c07085f295162a454fa358c47667e6b434ded8d
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2021-10-05 13:38:39 +0300

    use nginx-proxy instead of nginx manual redirection remove unused .env variables

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ ! -f /etc/nginx/ssl/default.crt ]; then
-    openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
-    openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
-    openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
-    chmod 644 /etc/nginx/ssl/default.key
+if [ ! -f /etc/nginx/certs/default.crt ]; then
+#    openssl req -x509 -out "/etc/nginx/certs/default.crt" -keyout "/etc/nginx/certs/default.key" \
+#            -newkey rsa:2048 -nodes -sha256 \
+#            -subj '/CN=localhost' -extensions EXT -config <( \
+#            printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+    openssl genrsa -out "/etc/nginx/certs/default.key" 2048
+    openssl req -new -key "/etc/nginx/certs/default.key" -out "/etc/nginx/certs/default.csr" -subj "/CN=default/O=default/C=UK"
+    openssl x509 -req -days 365 -in "/etc/nginx/certs/default.csr" -signkey "/etc/nginx/certs/default.key" -out "/etc/nginx/certs/default.crt"
+    chmod 644 /etc/nginx/certs/default.key
 fi
 
 # Start crond in background
